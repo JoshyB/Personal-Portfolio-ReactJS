daf1b7c11e77d53179a13c97b1de356a8c24e6f9
seejoshcode@gmail.com <seejoshcode@gmail.com>
2019-05-28 19:56:16 -0400

    changed where other examples were hosted

--- src/examples_data.js
+++ src/examples_data.js
@@ -3,7 +3,7 @@ const examples = {
     name: "GoodBoiCo",
     description: "Responsive experimental layout using CSS Grid",
     image: "/examples/screencaps/goodboico.jpg",
-    url: "/examples/goodboico/index.html"
+    url: "https://goodboico.herokuapp.com/"
   }
 };
 
